75484fdca290de9a9fd94b36a3a125e33def773f
JamJarLid <espen.lever@gmail.com>
2023-04-28 09:45:43 +0200

    different baseURL

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.use(express.static('lumiashop'));
 
-app.listen(5500, () => console.log('Listening on http://localhost:5500'));
+app.listen(5500, () => console.log('Listening on http://127.0.0.1:5500/'));
